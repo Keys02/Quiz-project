27b6b2f8f9ffcdb7d13a7c59ec893d226ee5292a
Keys <keys14052002@gmail.com>
2025-01-02 18:18:34 +0000

    Question set up done

---
+++ script.js
@@ -0,0 +1,51 @@
+let testQuestions = [
+    {
+        question: "Who is the CEO of SpaceX",
+        optionA: "Elon Musk",
+        optionB: "Sam Altman",
+        optionC: "Bill Gates",
+        optionD: "Jeff Bezos",
+        AnswerKey: "Elon Musk"
+    },
+    {
+        question: "Which of these technologies used for backend developement",
+        optionA: "CSS",
+        optionB: "HTML",
+        optionC: "Angular",
+        optionD: "PHP",
+        AnswerKey: "PHP"
+    },    {
+        question: "Who wrote the Harry Potter Series",
+        optionA: "J.K Rowling",
+        optionB: "Cal Newport",
+        optionC: "Jordan Peterson",
+        optionD: "Patrick Bet-David",
+        AnswerKey: "J.K. Rowling"
+    },    {
+        question: "Which of the following is an Object-Oriented Programming(OOP) language",
+        optionA: "JavaScript",
+        optionB: "Haskell",
+        optionC: "Java",
+        optionD: "Python",
+        AnswerKey: "Java"
+    },    {
+        question: "Who co-founded Microsoft",
+        optionA: "Mark Zuckerberg",
+        optionB: "Elon Musk",
+        optionC: "Bill Gates",
+        optionD: "Larry Ellison",
+        AnswerKey: "Elon Musk"
+    }
+]
+
+// for(let i = 0; i < testQuestions.length; i++) {
+//     $(".test-question").text(`${testQuestions[i]["question"]}`)
+//     $(".options-sec").append(`<input type="radio" id="html" name="option" value=${testQuestions[i]["optionA"]}>
+//                             <label for="html">${testQuestions[i]["optionA"]}</label><br>`)
+//     $(".options-sec").append(`<input type="radio" id="html" name="option" value=${testQuestions[i]["optionB"]}>
+//                             <label for="html">${testQuestions[i]["optionA"]}</label><br>`)
+//     $(".options-sec").append(`<input type="radio" id="html" name="option" value=${testQuestions[i]["optionC"]}>
+//                             <label for="html">${testQuestions[i]["optionA"]}</label><br>`)
+//     $(".options-sec").append(`<input type="radio" id="html" name="option" value=${testQuestions[i]["optionD"]}>
+//                             <label for="html">${testQuestions[i]["optionA"]}</label><br>`)                                
+// }
