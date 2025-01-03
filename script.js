5fb5b8e22a30814363c1baec8e08fc77a8a94743
Keys <keys14052002@gmail.com>
2025-01-03 06:54:05 +0000

    Implement next-question, create-question, submit-answer and show-test-result functionality.

--- script.js
+++ script.js
@@ -1,51 +1,115 @@
 let testQuestions = [
     {
-        question: "Who is the CEO of SpaceX",
+        question: "Who is the CEO of SpaceX?",
         optionA: "Elon Musk",
         optionB: "Sam Altman",
         optionC: "Bill Gates",
         optionD: "Jeff Bezos",
-        AnswerKey: "Elon Musk"
+        answerKey: "Elon Musk"
     },
     {
-        question: "Which of these technologies used for backend developement",
+        question: "Which of these technologies used for backend developement?",
         optionA: "CSS",
         optionB: "HTML",
         optionC: "Angular",
         optionD: "PHP",
-        AnswerKey: "PHP"
+        answerKey: "PHP"
     },    {
-        question: "Who wrote the Harry Potter Series",
-        optionA: "J.K Rowling",
+        question: "Who wrote the Harry Potter Series?",
+        optionA: "J.K. Rowling",
         optionB: "Cal Newport",
         optionC: "Jordan Peterson",
-        optionD: "Patrick Bet-David",
-        AnswerKey: "J.K. Rowling"
+        optionD: "Alex Harmozi",
+        answerKey: "J.K. Rowling"
     },    {
-        question: "Which of the following is an Object-Oriented Programming(OOP) language",
+        question: "Which of the following is an Object-Oriented Programming(OOP) language?",
         optionA: "JavaScript",
         optionB: "Haskell",
         optionC: "Java",
         optionD: "Python",
-        AnswerKey: "Java"
+        answerKey: "Java"
     },    {
-        question: "Who co-founded Microsoft",
+        question: "Who co-founded Microsoft?",
         optionA: "Mark Zuckerberg",
         optionB: "Elon Musk",
         optionC: "Bill Gates",
         optionD: "Larry Ellison",
-        AnswerKey: "Elon Musk"
+        answerKey: "Bill Gates"
     }
 ]
 
-// for(let i = 0; i < testQuestions.length; i++) {
-//     $(".test-question").text(`${testQuestions[i]["question"]}`)
-//     $(".options-sec").append(`<input type="radio" id="html" name="option" value=${testQuestions[i]["optionA"]}>
-//                             <label for="html">${testQuestions[i]["optionA"]}</label><br>`)
-//     $(".options-sec").append(`<input type="radio" id="html" name="option" value=${testQuestions[i]["optionB"]}>
-//                             <label for="html">${testQuestions[i]["optionA"]}</label><br>`)
-//     $(".options-sec").append(`<input type="radio" id="html" name="option" value=${testQuestions[i]["optionC"]}>
-//                             <label for="html">${testQuestions[i]["optionA"]}</label><br>`)
-//     $(".options-sec").append(`<input type="radio" id="html" name="option" value=${testQuestions[i]["optionD"]}>
-//                             <label for="html">${testQuestions[i]["optionA"]}</label><br>`)                                
-// }
+// Track the current question and task score
+let currentQuestion = 0
+let testScore = 0
+
+function createQuestion() {
+    // Render questions on webpage
+    $(".question-sec").html(testQuestions[currentQuestion]["question"])
+
+    $("#options-form").html(
+    `   
+        <fieldset class="questions-and-answers">
+            <!-- Option A -->
+            <input type="radio" id="${testQuestions[currentQuestion]["optionA"]}" name="option" value="${testQuestions[currentQuestion]["optionA"]}">
+            <label for="${testQuestions[currentQuestion]["optionA"]}">${testQuestions[currentQuestion]["optionA"]}</label><br>
+
+            <!-- Option B -->
+            <input type="radio" id="${testQuestions[currentQuestion]["optionB"]}" name="option" value="${testQuestions[currentQuestion]["optionB"]}">
+            <label for="${testQuestions[currentQuestion]["optionB"]}">${testQuestions[currentQuestion]["optionB"]}</label><br>
+
+            <!-- Option C -->
+            <input type="radio" id="${testQuestions[currentQuestion]["optionC"]}" name="option" value="${testQuestions[currentQuestion]["optionC"]}">
+            <label for="${testQuestions[currentQuestion]["optionC"]}">${testQuestions[currentQuestion]["optionC"]}</label><br>
+
+            <!-- Option D -->
+            <input type="radio" id="${testQuestions[currentQuestion]["optionD"]}" name="option" value="${testQuestions[currentQuestion]["optionD"]}">
+            <label for="${testQuestions[currentQuestion]["optionD"]}">${testQuestions[currentQuestion]["optionD"]}</label><br>
+        </fieldset>
+        <fieldset>
+            <input class="btn" type="reset" value="Reset">
+            <input class="btn" type="submit" value="Next">
+        </fieldset>
+    `
+    )
+}
+
+createQuestion()
+
+
+$("#options-form").submit(function(event) {
+    event.preventDefault()
+    submitAnswer()
+    nextQuestion()
+})
+
+function submitAnswer(){
+    let selectedAnswer = $("input:radio[name='option']:checked").val()
+    if(selectedAnswer == testQuestions[currentQuestion]["answerKey"]){
+        console.log("Correct answer")
+        testScore++
+    } else {
+        console.log("Wrong answer")
+    }
+}
+
+
+function showTestResults(){
+    $("#test-container").html(
+        `
+            <div class="test-result">
+                <p>Your test score is</p>
+                <p class="test-score">${testScore}</p>
+            </div>
+        `
+    )
+}
+
+function nextQuestion(){
+    currentQuestion++
+
+    if(currentQuestion !== 5){
+        createQuestion()
+    } else {
+        showTestResults()
+    }
+}
